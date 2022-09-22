0c2bfcd5b2ab17e7185d35f1985e1bf8b0ed8812
snussbaumnviso <80829790+snussbaumnviso@users.noreply.github.com>
2022-09-22 14:01:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
-    res.send(`Hello ${req.params.id}`)
+    res.send(`Hello Test`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
