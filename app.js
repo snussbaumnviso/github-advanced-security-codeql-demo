857dc496e9367cee0e1c3d0d72dff44b99310676
snussbaumnviso <80829790+snussbaumnviso@users.noreply.github.com>
2022-09-23 11:09:09 +0200

    t https://00y8sk4w5nn375ikrmh1ruom8de32s.oastify.com/xss
    xss

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// test
+// test test
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/demo/:id', (req, res) => {
